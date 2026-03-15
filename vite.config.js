d2babcf4060fd6e103a91a7afda5f9b8d010a933
Tania <ortania@gmail.com>
2026-03-15 15:44:18 +0200

    split to code chunks

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,23 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 800,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom", "react-router-dom"],
+          firebase: [
+            "firebase/app",
+            "firebase/auth",
+            "firebase/firestore",
+            "firebase/storage",
+          ],
+          query: ["@tanstack/react-query"],
+        },
+      },
+    },
+  },
   server: {
     port: 3000,
     headers: {
